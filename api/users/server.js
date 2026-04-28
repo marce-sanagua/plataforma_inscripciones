40a675e0549e1b5773a9d7004a6b43992cd5e02f
chericasta <celecastaa@gmail.com>
2026-04-28 02:00:02 -0300

    backend users funciona con MySQL

--- api/users/server.js
+++ api/users/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-// ROUTES
 const authRoutes = require("./src/routes/auth.routes");
-app.use("/", authRoutes);
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => res.json({ message: "Users service funcionando" }));
 
 app.listen(3000, () => {
   console.log("Users service en puerto 3000");
